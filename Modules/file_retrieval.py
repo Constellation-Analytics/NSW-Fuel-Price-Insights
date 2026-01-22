abd2c096b1dc6a3adf3e6f6686bc987b72e28502
Constellation-Analytics <paul.j.chapman@outlook.com>
2026-01-22 14:49:49 +0100

    Update file_retrieval.py

--- Modules/file_retrieval.py
+++ Modules/file_retrieval.py
@@ -3,14 +3,13 @@ import subprocess # to commit in GitHub repo
 import requests
 from bs4 import BeautifulSoup
 from datetime import datetime, timedelta
+import pandas as pd
+from io import BytesIO, StringIO
 
 url = "https://data.nsw.gov.au/data/dataset/fuel-check"
 response = requests.get(url)
 
-
 soup = BeautifulSoup(response.text, "html.parser")
-
-links = soup.find_all(class_='nsw-button nsw-button--dark resource-url-analytics',href=True)
 
 first_of_month = datetime.now().replace(day=1)
 last_month_date = first_of_month - timedelta(days=1)
@@ -20,23 +19,30 @@ last_month_year = last_month_date.strftime('%Y')
 year = last_month_year
 month = last_month_name
 
-download_link = [
-    href
+# Find links ending with .xlsx or .csv that match month/year (only one will be returned)
+download_links = [
+    a["href"]
     for a in soup.find_all("a", href=True)
-    if (href := a["href"].lower()).endswith(".xlsx")
+    if (href := a["href"].lower()).endswith((".xlsx", ".csv"))
     and year in href
     and month in href
 ]
 
-#print(download_link)
+link = download_links[0]
+print("Downloading:", link)
+
+resp = requests.get(link)
 
 # Make sure the folder exists
 os.makedirs("Data and Logs", exist_ok=True)
 
-# Example: save a file
-with open("Data and Logs/fuelcheck_dec2025.xlsx", "wb") as f:
-    f.write(b"Example content")  # Replace with real downloaded content
-
+# Read file based on extension
+if link.endswith(".xlsx"):
+    df = pd.read_excel(BytesIO(resp.content))
+elif link.endswith(".csv"):
+    df = pd.read_csv(StringIO(resp.content.decode('utf-8')))
+    
+df.to_csv(f"Data and Logs/fuelcheck_{month}{year}.csv", index=False)
 
 # Configure git to use GitHub Actions token
 repo_url = f"https://x-access-token:{os.environ['GITHUB_TOKEN']}@github.com/{os.environ['GITHUB_REPOSITORY']}.git"
