4b2e8822826579514488eb8105cb187f5bcc0e1d
Constellation-Analytics <paul.j.chapman@outlook.com>
2026-01-23 13:07:40 +0100

    Update orchestrator.py

--- orchestrator.py
+++ orchestrator.py
@@ -15,7 +15,7 @@ log_file = f"data and logs/workflow{datetimestamp}.log"
 logging.basicConfig(
   filename=log_file,
   level=logging.INFO,
-  format="%(asctime)s - Orchestrator - %(levelname)s - %(message)s",
+  format="%(asctime)s - %(levelname)s - Orchestrator - %(message)s",
   datefmt="%Y-%m-%d %H:%M:%S"
 )
 
