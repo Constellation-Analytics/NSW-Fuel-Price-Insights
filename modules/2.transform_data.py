ba8176ab8aa6efd750a48c15c6252648252ecac6
Constellation-Analytics <paul.j.chapman@outlook.com>
2026-02-21 12:15:02 +0100

    Fix formatting in logging setup

--- modules/2.transform_data.py
+++ modules/2.transform_data.py
@@ -27,7 +27,7 @@ os.makedirs("data and logs", exist_ok=True)
 logging.basicConfig(
     filename=log_file,
     level=logging.INFO,
-    format="%(asctime)s - %(levelname)s -      Module 2     - %(message)s",
+    format="%(asctime)s - %(levelname)s -      Module 2    - %(message)s",
     datefmt="%Y-%m-%d %H:%M:%S"
 )
 
