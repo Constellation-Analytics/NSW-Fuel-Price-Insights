4482210ad64c1f99c0a7141cd1623639ffb7b909
Constellation-Analytics <paul.j.chapman@outlook.com>
2026-02-21 12:32:07 +0100

    Refactor data_quality.py to remove config handling
    Removed JSON config handling and related functions. Simplified script by eliminating unused imports and variables.

---
+++ modules/4.data_quality.py
@@ -0,0 +1,50 @@
+# Import packages
+# Import necessary libraries
+from sqlalchemy import create_engine, text
+import argparse
+import logging
+import os
+import subprocess
+
+# ----------------------------------------------------------------------------------------------------
+#                                       setup variables
+# ----------------------------------------------------------------------------------------------------
+
+# Get log file path from orchestrator
+parser = argparse.ArgumentParser()
+parser.add_argument("--log-file", required=True)
+args = parser.parse_args()
+log_file = args.log_file
+
+os.makedirs("data and logs", exist_ok=True)
+
+# Set up logging for module
+logging.basicConfig(
+    filename=log_file,
+    level=logging.INFO,
+    format="%(asctime)s - %(levelname)s -      Module 3    - %(message)s",
+    datefmt="%Y-%m-%d %H:%M:%S"
+)
+
+# Create logger with dummy name so it can be scaled later if needed
+logger = logging.getLogger("log_dog")
+
+# Load environment variables from GitHub Secrets
+DB_CONNECTION_STRING = os.getenv("DB_CONNECTION_STRING")
+
+# Create database engine
+engine = create_engine(DB_CONNECTION_STRING)
+
+
+# ----------------------------------------------------------------------------------------------------
+#                                     Script Body - Start
+# ----------------------------------------------------------------------------------------------------
+
+logger.info("Running SQL Stored Procedure")
+
+call = text("CALL truncate_stg_stations();")
+with engine.connect() as conn:
+    conn = conn.execution_options(isolation_level="AUTOCOMMIT")
+    conn.execute(call)
+
+logger.info("Operation complete")
