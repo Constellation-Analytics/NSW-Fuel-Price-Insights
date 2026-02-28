b266e37b2aa4c8b0bfb71935d71b3248224acc83
Constellation-Analytics <paul.j.chapman@outlook.com>
2026-02-28 21:48:27 +0100

    Update 4.data_quality.py

--- modules/4.data_quality.py
+++ modules/4.data_quality.py
@@ -96,7 +96,7 @@ sys_run_log_dbo = pd.read_sql(data_quality_query, engine)
 
 for index, row in sys_run_log_dbo.iterrows():
     logger.warning(
-        'defect_id %s has %d rows',
+        'Autofix %s has %d rows',
         row["description"],
         row["rows_affected"]
     )
