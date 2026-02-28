4681771fad67d3c774fab4a9928cd0b775d174bb
Constellation-Analytics <paul.j.chapman@outlook.com>
2026-02-28 22:02:46 +0100

    Update 4.data_quality.py

--- modules/4.data_quality.py
+++ modules/4.data_quality.py
@@ -65,7 +65,7 @@ sys_run_log_dbo = pd.read_sql(data_quality_query, engine)
 
 for index, row in sys_run_log_dbo.iterrows():
     logger.warning(
-        'defect_id %s has %d rows',
+        'dq_id %s has %d rows',
         row["description"],
         row["rows_affected"]
     )
@@ -127,7 +127,7 @@ sys_run_log_dbo = pd.read_sql(data_quality_query, engine)
 
 for index, row in sys_run_log_dbo.iterrows():
     logger.warning(
-        'defect_id %s has %d rows',
+        'dq_id %s has %d rows',
         row["description"],
         row["rows_affected"]
     )
