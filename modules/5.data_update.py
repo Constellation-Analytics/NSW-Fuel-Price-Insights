71551050ead25c4dcfc880988e835257c5918223
Constellation-Analytics <paul.j.chapman@outlook.com>
2026-03-01 12:18:48 +0100

    Update 5.data_update.py

--- modules/5.data_update.py
+++ modules/5.data_update.py
@@ -8,6 +8,7 @@ import logging
 import os
 import pandas as pd
 import subprocess
+import sys
 
 # ----------------------------------------------------------------------------------------------------
 #                                       setup variables
