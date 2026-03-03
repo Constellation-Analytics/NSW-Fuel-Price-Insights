82e749d41f6ba87e01d55f174b1a66ceec148f12
Constellation-Analytics <paul.j.chapman@outlook.com>
2026-03-03 13:31:51 +0100

    Update 2.transform_data.py

--- modules/2.transform_data.py
+++ modules/2.transform_data.py
@@ -364,6 +364,7 @@ try:
 
 except Exception as e:
     logger.exception(f"Unexpected error while inserting values into database: {e}")
+	sys.exit(10)
 
 #update the config 
 config["last_transformation"] = config["latest_file"]
